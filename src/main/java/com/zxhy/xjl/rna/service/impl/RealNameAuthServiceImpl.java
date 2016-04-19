db2dd8dd1d0e53062369104cdb2d6447bf3d67d4
guanzheyuan <834155417@qq.com>
2016-04-19 09:05:22 +0800

    doUpdate

--- src/main/java/com/zxhy/xjl/rna/service/impl/RealNameAuthServiceImpl.java
+++ src/main/java/com/zxhy/xjl/rna/service/impl/RealNameAuthServiceImpl.java
@@ -16,14 +16,7 @@ public class RealNameAuthServiceImpl implements RealNameAuthService {
 		realNameAuth.setPasswd(passwd);
 		mapper.save(realNameAuth);
 	}
-	public void updateRealName(String phone, String idCode, String idName) {
-		RealNameAuth realNameAuth = new RealNameAuth();
-		realNameAuth.setPhone(phone);
-		realNameAuth.setIdCode(idCode);
-		realNameAuth.setIdName(idName);
-		this.mapper.updateRealName(realNameAuth);
-		
-	}
+	
 	public void updatePhoto(String phone, String photoUrl){
 		RealNameAuth realNameAuth = new RealNameAuth();
 		realNameAuth.setPhone(phone);
@@ -31,6 +24,7 @@ public class RealNameAuthServiceImpl implements RealNameAuthService {
 		this.mapper.updatePhoto(realNameAuth);
 		
 	}
+	
 	public void updateFace(String phone, String faceUrl) {
 		RealNameAuth realNameAuth = new RealNameAuth();
 		realNameAuth.setPhone(phone);
@@ -38,11 +32,21 @@ public class RealNameAuthServiceImpl implements RealNameAuthService {
 		this.mapper.updateFace(realNameAuth);
 		
 	}
+	
 	public void delete(String phone) {
 		this.mapper.delete(phone);
 	}
+	
 	public RealNameAuth findByPhone(String phone) {
 		return this.mapper.findByPhone(phone);
 	}
 	
+	@Override
+	public void updateRealName(String phone, String idCode, String idName) {
+		RealNameAuth realNameAuth = new RealNameAuth();
+		realNameAuth.setPhone(phone);
+		realNameAuth.setIdCode(idCode);
+		realNameAuth.setIdName(idName);
+		this.mapper.updateRealName(realNameAuth);
+	}
 }
