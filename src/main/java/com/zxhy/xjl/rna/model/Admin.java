9fdae646a908799dd5c37302dad6209f92ffaa34
yangzaixiong <1981975614@qq.com>
2016-04-27 17:36:16 +0800

    管理员登录

---
+++ src/main/java/com/zxhy/xjl/rna/model/Admin.java
@@ -0,0 +1,33 @@
+package com.zxhy.xjl.rna.model;
+
+/**
+ * 管理员 
+ * @author   yangzaixiong
+ * 
+ */
+public class Admin {
+	private String oid;
+	private String accountNumber;
+	private String passwd;
+	
+	
+	public String getOid() {
+		return oid;
+	}
+	public void setOid(String oid) {
+		this.oid = oid;
+	}
+	public String getAccountNumber() {
+		return accountNumber;
+	}
+	public void setAccountNumber(String accountNumber) {
+		this.accountNumber = accountNumber;
+	}
+	public String getPasswd() {
+		return passwd;
+	}
+	public void setPasswd(String passwd) {
+		this.passwd = passwd;
+	}
+	
+}
